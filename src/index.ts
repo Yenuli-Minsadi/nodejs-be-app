372fa4b77d2093f418ebb430d11ff551393f3e6d
Yenuli Minsadi <samararatney@gmail.com>
2026-04-10 11:11:51 +0530

    update with post method

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
-import express from "express"
+import express, { Request, Response } from "express"
 
 const app = express()
 
+// middleware - like filters in springboot, to handle data as json before handling req
+//middleware to parse JSON bodies
+app.use(express.json())
+
 // GET Req, send method eka thru response ekk ywnw
 app.get("/", (req,res) => {
+    console.log("req came")
     res.send("Hello, express")
+})
+
+app.post("/api/v1/save", (req: Request, res: Response) => {
+    const data = req.body //w/ou the middleware methanna data allgnna ba, allgnna wenne na
+    console.log("Body data: ", data)
+    res.status(200).json({
+        message: "Data received successfully!",
+        data
+    })
 })
 
 // create server
